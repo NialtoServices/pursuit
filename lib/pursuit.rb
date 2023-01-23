47ab30881d0acabce2aec753df14fae68b2a8487
Toby Herbert <tobyherbert@me.com>
2023-01-23 13:36:59 +0000

    Removed the previous search implementation

--- lib/pursuit.rb
+++ lib/pursuit.rb
@@ -1,7 +1,3 @@
 # frozen_string_literal: true
 
 require_relative 'pursuit/constants'
-require_relative 'pursuit/search_options'
-require_relative 'pursuit/search_term_parser'
-require_relative 'pursuit/search'
-require_relative 'pursuit/railtie' if defined?(Rails::Railtie)
--- spec/internal/app/models/product.rb
+++ spec/internal/app/models/product.rb
@@ -6,17 +6,4 @@ class Product < ActiveRecord::Base
   has_many :variations, class_name: 'ProductVariation', inverse_of: :product
 
   validates :title, presence: true
-
-  searchable do |o|
-    o.relation :variations, :title, :stock_status
-
-    o.attribute :title
-    o.attribute :description
-    o.attribute :rating, unkeyed: false
-    o.attribute :title_length, unkeyed: false do
-      Arel::Nodes::NamedFunction.new('LENGTH', [arel_table[:title]])
-    end
-
-    o.attribute :category, :category_id
-  end
 end
