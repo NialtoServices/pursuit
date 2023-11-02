5b27a386e1bd7e943126c85ac48edb3d52053a8e
Toby Herbert <tobyherbert@nialtoservices.co.uk>
2023-11-02 16:49:30 +0000

    Added example search methods to the spec records

--- spec/internal/app/models/product_category.rb
+++ spec/internal/app/models/product_category.rb
@@ -4,4 +4,26 @@ class ProductCategory < ApplicationRecord
   has_many :products, class_name: 'Product', foreign_key: :category_id, inverse_of: :category, dependent: :nullify
 
   validates :name, presence: true
+
+  def self.predicate_search
+    @predicate_search ||= Pursuit::PredicateSearch.new(
+      left_outer_joins(:products).group(:id)
+    ) do
+      permit_attribute :name
+      permit_attribute :product, Product.arel_table[:id]
+      permit_attribute :product_title, Product.arel_table[:title]
+    end
+  end
+
+  def self.term_search
+    @term_search ||= Pursuit::TermSearch.new(all) do
+      search_attribute :name
+    end
+  end
+
+  def self.search(query)
+    predicate_search.apply(query)
+  rescue Parslet::ParseFailed
+    term_search.apply(query)
+  end
 end
